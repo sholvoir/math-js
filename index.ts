f4388018b2080ab08d581d53df0ed2635bb90550
Shaw He <shaw@c02y343wjgh6.attlocal.net>
2020-03-13 21:11:35 -0400

    temp

--- index.ts
+++ index.ts
@@ -4,9 +4,9 @@ class Point {
 }
 
 class Line {
-    constructor(public a: number, public b: number) {}
-    toString() { return `y = ${this.a}x${this.b === 0 ? '' : this.b > 0 ? ` + ${this.b}` : ` - ${-this.b}`}` }
-} 
+    constructor(public a: number, public b: number, public c: number) {}
+    toString() { return `${this.a}x + ${this.b}y = ${this.c}${this.b == 0 ? '' : ` (y = ${-this.a/this.b}x + ${this.c/this.b})`}` }
+}
 
 const mean = (data: Array<number>) => data.reduce((acc, cur) => acc + cur) / data.length;
 const mad = (data: Array<number>) => { const average = mean(data); return mean(data.map(x => Math.abs(x - average))); }
@@ -18,8 +18,8 @@ const volumeOfCylinder = (radius: number, height: number) => Math.PI * radius * 
 const hypotenuse = (a: number, b: number) => Math.sqrt(a * a + b * b);
 const missSide = (b: number, c: number) => Math.sqrt(c * c - b * b);
 const suerfaceAreaOfRectangularPrism = (a, b, c) => 2 * (a * b + b * c + a * c);
-const lineFromTwoPoint = ({x: x1, y: y1}: Point, { x: x2, y: y2}: Point) => new Line((y2-y1)/(x2-x1), (x2*y1-x1*y2)/(x2-x1));
-const intersectionOfTwoLine = ({a: a1, b: b1}: Line, {a: a2, b: b2}: Line) => new Point((b1-b2)/(a2-a1), (a2*b1-a1*b2)/(a2-a1));
+const lineFromTwoPoint = ({x: x1, y: y1}: Point, { x: x2, y: y2}: Point) => new Line(y2-y1, x1-x2, x1*y2-x2*y1);
+const intersectionOfTwoLine = ({a: a1, b: b1, c: c1}: Line, {a: a2, b: b2, c: c2}: Line) => new Point((c1*b2-c2*b1)/(a1*b2-a2*b1), (a1*c2-a2*c1)/(a1*b2-a2*b1));
 
 const functions = {
     mean,
@@ -32,7 +32,7 @@ const functions = {
     ms: missSide,
     sarp: suerfaceAreaOfRectangularPrism,
     l: (x1, y1, x2, y2) => lineFromTwoPoint(new Point(x1, y1), new Point(x2, y2)).toString(),
-    i: (a1, b1, a2, b2) => intersectionOfTwoLine(new Line(a1, b1), new Line(a2, b2)).toString(),
+    i: (a1, b1, c1, a2, b2, c2) => intersectionOfTwoLine(new Line(a1, b1, c1), new Line(a2, b2, c2)).toString(),
     i2: (xa, ya, xb, yb, xc, yc, xd, yd) => intersectionOfTwoLine(lineFromTwoPoint(new Point(xa, ya), new Point(xb, yb)), lineFromTwoPoint(new Point(xc, yc), new Point(xd, yd))).toString()
 }
 
